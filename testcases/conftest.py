bddfddb6bc2cd4ad19e6484abfe949b1b265ffa1
kinopico <zqmillet@qq.com>
2022-02-06 16:54:32 +0800

    [Test] add a fixture named proxies.

--- testcases/conftest.py
+++ testcases/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+import urllib
 
 def pytest_addoption(parser):
     parser.addoption(
@@ -6,7 +7,7 @@ def pytest_addoption(parser):
         action='store',
         type=str,
         help='specify the app id of baidu translation service',
-        default=None
+        required=True
     )
 
     parser.addoption(
@@ -14,7 +15,39 @@ def pytest_addoption(parser):
         action='store',
         type=str,
         help='specify the app key of baidu translation service',
-        default=None
+        required=True
+    )
+
+    parser.addoption(
+        '--proxy-host',
+        action='store',
+        type=str,
+        help='specify the host of proxy',
+        default=None,
+    )
+
+    parser.addoption(
+        '--proxy-port',
+        action='store',
+        type=str,
+        help='specify the port of proxy',
+        default=None,
+    )
+
+    parser.addoption(
+        '--proxy-username',
+        action='store',
+        type=str,
+        help='specify the username of proxy',
+        default=None,
+    )
+
+    parser.addoption(
+        '--proxy-password',
+        action='store',
+        type=str,
+        help='specify the password of proxy',
+        default=None,
     )
 
 @pytest.fixture(name='app_id', scope='session')
@@ -24,3 +57,38 @@ def _app_id(request):
 @pytest.fixture(name='app_key', scope='session')
 def _app_key(request):
     return request.config.getoption('app_key')
+
+@pytest.fixture(name='proxy_host', scope='session')
+def _proxy_host(request):
+    return request.config.getoption('proxy_host')
+
+@pytest.fixture(name='proxy_port', scope='session')
+def _proxy_port(request):
+    return request.config.getoption('proxy_port')
+
+@pytest.fixture(name='proxy_username', scope='session')
+def _proxy_username(request):
+    return request.config.getoption('proxy_username')
+
+@pytest.fixture(name='proxy_password', scope='session')
+def _proxy_password(request):
+    return request.config.getoption('proxy_password')
+
+@pytest.fixture(name='proxies', scope='session')
+def _proxies(proxy_host, proxy_port, proxy_username, proxy_password):
+    if not proxy_host or not proxy_port:
+        return {
+            'http': None,
+            'https': None
+        }
+
+    if not proxy_username or not proxy_password:
+        return {
+            'http': f'http://{urllib.parse.quote(proxy_username)}:{urllib.parse.quote(proxy_password)}@{proxy_host}:{proxy_port}',
+            'https': f'http://{urllib.parse.quote(proxy_username)}:{urllib.parse.quote(proxy_password)}@{proxy_host}:{proxy_port}',
+        }
+
+    return {
+        'http': f'http://{proxy_host}:{proxy_port}',
+        'https': f'http://{proxy_host}:{proxy_port}',
+    }
