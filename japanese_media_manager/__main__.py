783027ab8c65a7516dca9eb5d342aa4d4a61cf43
kinopico <zqmillet@qq.com>
2022-02-08 20:13:50 +0800

    [Feature] add __main__.py file.

---
+++ japanese_media_manager/__main__.py
@@ -0,0 +1,25 @@
+import argparse
+
+from japanese_media_manager.scripts.generate_configuration import generate_configuration
+
+class COMMAND:
+    GENERATE_CONFIG = 'generate-config'
+    SCRAPE = 'scrape'
+
+argument_parser = argparse.ArgumentParser(
+    prog='jmm',
+    description='collect, check and complete your personal adult videos'
+)
+
+subparsers = argument_parser.add_subparsers(dest='command')
+generate_config_parser = subparsers.add_parser(COMMAND.GENERATE_CONFIG)
+scrape_parser = subparsers.add_parser(COMMAND.SCRAPE)
+
+arguments = argument_parser.parse_args()
+
+if arguments.command == COMMAND.GENERATE_CONFIG:
+    generate_configuration()
+elif arguments.command == COMMAND.SCRAPE:
+    pass
+else:
+    argument_parser.print_usage()
---
+++ japanese_media_manager/scripts/generate_configuration.py
@@ -0,0 +1,20 @@
+import os
+import pathlib
+
+def get_answer(text, default=True):
+    while True:
+        answer = input(text).lower() or 'y'
+        if answer in ['y', 'n']:
+            return answer == 'y'
+
+def generate_configuration(file_path=os.path.join(pathlib.Path.home(), '.jmm.cfg')):
+    with open(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.config.yaml'), 'r', encoding='utf8') as file:
+        configuration = file.read()
+
+    if os.path.isfile(file_path) and not get_answer(f'file {file_path} exists, overwrite it? (Y/n) '):
+        return
+
+    with open(file_path, 'w', encoding='utf8') as file:
+        file.write(configuration)
+
+    print(f'configuration has been saved in file {file_path}')
