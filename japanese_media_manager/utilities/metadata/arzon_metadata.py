47ad5fc18399a8715980d5431863d0db603d7b56
kinopico <zqmillet@qq.com>
2022-02-09 22:08:37 +0800

    [Feature] add some function into class ArzonMetaData class.

--- japanese_media_manager/utilities/metadata/arzon_metadata.py
+++ japanese_media_manager/utilities/metadata/arzon_metadata.py
@@ -7,6 +7,12 @@ from .base import Base
 
 class TAG:
     RELEASE_DATE = '発売日：'
+    LENGHT = '収録時間：'
+    NUMBER = '品番：'
+    DIRECTOR = '監督：'
+    SERIES = 'AVレーベル：'
+    STUDIO = 'AVメーカー：'
+    STARTS = 'AV女優：'
 
 class ArzonMetaData(Base):
     session = None
@@ -59,22 +65,66 @@ class ArzonMetaData(Base):
             return
 
     def load_length(self):
-        pass
+        for tag in self.soup.find_all('td'):
+            if not tag.text == TAG.LENGHT:
+                continue
+
+            match = re.match(pattern=r'(?P<length>\d+)(?P<unit>\w+)', string=tag.find_next('td').text.strip())
+            if not match:
+                continue
+            self.length = (match.groupdict()['length'], match.groupdict()['unit'])
+            return
 
     def load_number(self):
-        pass
+        for tag in self.soup.find_all('strong'):
+            if not tag.text == TAG.NUMBER:
+                continue
+            self.number = tag.next.next.strip()
+            return
 
     def load_director(self):
-        pass
+        for tag in self.soup.find_all('td'):
+            if not tag.text == TAG.DIRECTOR:
+                continue
+
+            self.director = tag.find_next('td').text.strip()
+            return
 
     def load_series(self):
-        pass
+        for tag in self.soup.find_all('td'):
+            if not tag.text == TAG.SERIES:
+                continue
+
+            self.series = tag.find_next('td').text.strip()
+            return
 
     def load_studio(self):
-        pass
+        for tag in self.soup.find_all('td'):
+            if not tag.text == TAG.STUDIO:
+                continue
+
+            self.studio = tag.find_next('td').text.strip()
+            return
 
     def load_stars(self):
-        pass
+        for tag in self.soup.find_all('td'):
+            if not tag.text == TAG.STARTS:
+                continue
+
+            for link in tag.find_next('td').find_all('a'):
+                response = self.session.get(f'{self.base_url}{link.attrs["href"]}', proxies=self.proxies)
+                response.encoding = 'utf8'
+                soup = bs4.BeautifulSoup(response.text, 'html.parser')
+
+                for item in soup.find_all('table', 'p_list1'):
+                    image = item.find_next('img')
+                    self.stars.append(
+                        {
+                            'name': image.attrs['alt'],
+                            'avatar_url': f'https:{image.attrs["src"]}'
+                        }
+                    )
 
     def load_outline(self):
-        pass
+        for tag in self.soup.find_all('h2'):
+            import pdb; pdb.set_trace()
