9c6d1fe3be9faed6f6094dfce70fa6a379411744
kinopico <zqmillet@qq.com>
2022-02-10 20:19:04 +0800

    [Test] polish the flaky testcases.

--- testcases/utilities/session/session_test.py
+++ testcases/utilities/session/session_test.py
@@ -4,29 +4,32 @@ import pytest
 from japanese_media_manager.utilities.session import Session
 from japanese_media_manager.utilities.timer import Timer
 
+url = 'https://www.baidu.com'
+threthold = 0.5
+
 @pytest.mark.parametrize('interval', [1, 1.5, 0.5, 0])
 def test_session(interval):
     session = Session(interval=interval)
 
     with Timer() as timer:
-        session.get('https://www.baidu.com')
-    assert timer.time < 0.1
+        session.get(url)
+    assert timer.time < threthold
 
     for _ in range(4):
         with Timer() as timer:
-            session.get('https://www.baidu.com')
-        assert timer.time - interval < 0.1
+            session.get(url)
+        assert timer.time - interval < threthold
 
 @pytest.mark.parametrize('interval', [1, 1.5, 0.5, 0])
 def test_session_with_sleep(interval):
     session = Session(interval=interval)
 
     with Timer() as timer:
-        session.get('https://www.baidu.com')
-    assert timer.time < 0.1
+        session.get(url)
+    assert timer.time < threthold
 
     time.sleep(interval)
 
     with Timer() as timer:
-        session.get('https://www.baidu.com')
-    assert timer.time < 0.1
+        session.get(url)
+    assert timer.time < threthold
