d64a228c4ec3bfa0ca70c61dae2179960fbc3ff6
kinopico <zqmillet@qq.com>
2022-02-13 12:58:37 +0800

    [Test] finish testcases about class Session.

--- japanese_media_manager/utilities/session/session.py
+++ japanese_media_manager/utilities/session/session.py
@@ -1,9 +1,16 @@
 import time
+import http
 import urllib3
 import requests
 
+def get_retry_class(interval):
+    class Retry(urllib3.util.Retry):
+        def sleep(self, response=None):
+            time.sleep(interval)
+    return Retry
+
 class Session(requests.Session):
-    def __init__(self, *args, interval=1, timeout=None, proxies=None, retries=3, **kwargs):
+    def __init__(self, *args, interval=0, timeout=None, proxies=None, retries=3, **kwargs):
         self.interval = interval
         self.timeout = timeout
 
@@ -11,13 +18,18 @@ class Session(requests.Session):
         self.proxies.update(proxies or {'http': None, 'https': None})
         self.last_access_time = 0
 
-        adapter = requests.adapters.HTTPAdapter(
-            max_retries=urllib3.util.Retry(
-                total=retries,
-                allowed_methods=['GET', 'POST'],
-                status_forcelist=[500]
-            )
+        retry = get_retry_class(interval)(
+            total=retries,
+            allowed_methods=["HEAD", "GET", "POST", "PUT", "DELETE", "OPTIONS", "TRACE"],
+            status_forcelist=[
+                http.HTTPStatus.BAD_GATEWAY,
+                http.HTTPStatus.BAD_REQUEST,
+                http.HTTPStatus.INTERNAL_SERVER_ERROR,
+                http.HTTPStatus.FORBIDDEN,
+            ],
         )
+
+        adapter = requests.adapters.HTTPAdapter(max_retries=retry)
 
         self.mount('https://', adapter)
         self.mount('http://', adapter)
@@ -25,4 +37,5 @@ class Session(requests.Session):
     def request(self, *args, **kwargs):
         time.sleep(max(0, self.interval - time.time() + self.last_access_time))
         self.last_access_time = time.time()
-        return super().request(*args, **kwargs, timeout=self.timeout)
+        kwargs['timeout'] = kwargs.get('timeout') or self.timeout
+        return super().request(*args, **kwargs)
--- testcases/utilities/session/session_test.py
+++ testcases/utilities/session/session_test.py
@@ -1,22 +1,28 @@
 import http
 import os
+import time
 import signal
 import multiprocessing
 import contextlib
+import logging
 import tornado.web
 import tornado.ioloop
 import requests
 import pytest
 
 from japanese_media_manager.utilities.session import Session
+from japanese_media_manager.utilities.timer import Timer
 
-def start_tornado(api_path, port, method, responses, queue):
+def start_tornado(api_path, port, method, responses, wait, queue):
+    logging.getLogger('tornado.access').disabled = True
+
     class RequestHandler(tornado.web.RequestHandler): # pylint: disable = abstract-method
         response_index = 0
 
     def _method(self, *args, **kwargs): # pylint: disable = unused-argument
         response = responses[RequestHandler.response_index % len(responses)]
         RequestHandler.response_index += 1
+        time.sleep(wait)
         self.set_status(response.get('status_code', http.HTTPStatus.OK))
         self.finish(response['response'])
 
@@ -33,9 +39,9 @@ def start_tornado(api_path, port, method, responses, queue):
         tornado.ioloop.IOLoop.current().stop()
 
 @contextlib.contextmanager
-def mock_server_manager(api_path, responses, port=8001, method='get'):
+def mock_server_manager(api_path, responses, port=8001, method='get', wait=0):
     queue = multiprocessing.Queue()
-    process = multiprocessing.Process(target=start_tornado, args=(api_path, port, method, responses, queue), daemon=True)
+    process = multiprocessing.Process(target=start_tornado, args=(api_path, port, method, responses, wait, queue), daemon=True)
     process.start()
     queue.get()
     yield
@@ -44,7 +50,8 @@ def mock_server_manager(api_path, responses, port=8001, method='get'):
 
 def test_session():
     api_path = r'/book'
-    url = f'http://localhost:{8001}{api_path}'
+    port = 8001
+    url = f'http://localhost:{port}{api_path}'
 
     responses = [
         {'response': 'test_session', 'status_code': http.HTTPStatus.OK},
@@ -54,15 +61,19 @@ def test_session():
         response = session.get(url)
         assert response.status_code == http.HTTPStatus.OK
 
-def test_session_with_retry():
+@pytest.mark.parametrize(
+    'status_code', [http.HTTPStatus.INTERNAL_SERVER_ERROR, http.HTTPStatus.BAD_REQUEST, http.HTTPStatus.BAD_GATEWAY]
+)
+def test_session_with_retry(status_code):
     api_path = r'/book'
-    url = f'http://localhost:{8001}{api_path}'
+    port = 8001
+    url = f'http://localhost:{port}{api_path}'
 
     responses = [
-        {'response': 'test_session_with_retry', 'status_code': http.HTTPStatus.INTERNAL_SERVER_ERROR},
-        {'response': 'test_session_with_retry', 'status_code': http.HTTPStatus.INTERNAL_SERVER_ERROR},
-        {'response': 'test_session_with_retry', 'status_code': http.HTTPStatus.INTERNAL_SERVER_ERROR},
-        {'response': 'test_session_with_retry', 'status_code': http.HTTPStatus.INTERNAL_SERVER_ERROR},
+        {'response': 'test_session_with_retry', 'status_code': status_code},
+        {'response': 'test_session_with_retry', 'status_code': status_code},
+        {'response': 'test_session_with_retry', 'status_code': status_code},
+        {'response': 'test_session_with_retry', 'status_code': status_code},
         {'response': 'test_session_with_retry', 'status_code': http.HTTPStatus.OK},
     ]
     with mock_server_manager(api_path=api_path, responses=responses):
@@ -78,3 +89,71 @@ def test_session_with_retry():
     with mock_server_manager(api_path=api_path, responses=responses):
         with pytest.raises(requests.exceptions.RetryError):
             Session(retries=1).get(url)
+
+def test_session_with_timeout():
+    api_path = r'/book'
+    port = 8001
+    url = f'http://localhost:{port}{api_path}'
+
+    responses = [
+        {'response': 'test_session_with_timeout', 'status_code': http.HTTPStatus.OK},
+    ]
+
+    with mock_server_manager(api_path=api_path, responses=responses):
+        session = Session()
+        with Timer() as timer:
+            session.get(url)
+        assert timer.time < 0.1
+
+    with mock_server_manager(api_path=api_path, responses=responses, wait=1):
+        session = Session()
+        with Timer() as timer:
+            session.get(url)
+        assert 1.0 < timer.time < 1.1
+
+    with mock_server_manager(api_path=api_path, responses=responses, wait=1):
+        session = Session(timeout=0.5)
+
+        with pytest.raises(requests.exceptions.RequestException) as information:
+            session.get(url)
+        assert 'timeout' in str(information.value)
+
+        session.get(url, timeout=2)
+
+    with mock_server_manager(api_path=api_path, responses=responses, wait=1):
+        session = Session(timeout=2)
+
+        session.get(url)
+        with pytest.raises(requests.exceptions.RequestException) as information:
+            session.get(url, timeout=0.5)
+        assert 'timeout' in str(information.value)
+
+def test_session_with_interval():
+    api_path = r'/book'
+    port = 8001
+    url = f'http://localhost:{port}{api_path}'
+
+    responses = [{'response': 'test_session_with_timeout', 'status_code': http.HTTPStatus.OK}]
+
+    with mock_server_manager(api_path=api_path, responses=responses):
+        session = Session(interval=1)
+
+        with Timer() as timer:
+            session.get(url)
+            session.get(url)
+        assert 1 < timer.time < 1.1
+
+    responses = [{'response': 'test_session_with_timeout', 'status_code': http.HTTPStatus.INTERNAL_SERVER_ERROR}]
+    with mock_server_manager(api_path=api_path, responses=responses):
+        session = Session(interval=1, retries=3)
+
+        with Timer() as timer:
+            with pytest.raises(requests.exceptions.RequestException):
+                session.get(url)
+
+        assert 3 < timer.time < 3.1
+
+def test_session_with_proxies(proxies):
+    session = Session(proxies=proxies)
+    response = session.get('https://www.google.com')
+    assert response.status_code == http.HTTPStatus.OK
