1c254ae900c1ae8ea5d11dc5fdf728232ae98d36
kinopico <zqmillet@qq.com>
2022-03-01 16:43:20 +0800

    [Refactor] polish code according to pylint and flake8.

--- testcases/scripts/entry_point_test.py
+++ testcases/scripts/entry_point_test.py
@@ -2,22 +2,21 @@ import sys
 import subprocess
 import pytest
 
-import japanese_media_manager.scripts.command as COMMAND
-
 def test_help():
     cmd = [sys.executable, '-m', 'japanese_media_manager', '-h']
-    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    output, _ = process.communicate()
-    assert 'usage: jmm' in output.decode('utf8')
+    with subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT) as process:
+        output, _ = process.communicate()
+        assert 'usage: jmm' in output.decode('utf8')
 
 @pytest.mark.parametrize('command', ['xxx', 'tiny-work', '2333'])
 def test_wrong_command(command):
     cmd = [sys.executable, '-m', 'japanese_media_manager', command]
-    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    output, _ = process.communicate()
+    with subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT) as process:
+        output, _ = process.communicate()
+        assert f"jmm: error: argument command: invalid choice: '{command}'" in output.decode('utf8')
 
 def test_none_command():
     cmd = [sys.executable, '-m', 'japanese_media_manager']
-    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    output, _ = process.communicate()
-    assert 'usage: jmm' in output.decode('utf8')
+    with subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT) as process:
+        output, _ = process.communicate()
+        assert 'usage: jmm' in output.decode('utf8')
