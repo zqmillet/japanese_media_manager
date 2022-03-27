baf4cbfeb9974635c71f50c1d33605bac82bf0fe
kinopico <zqmillet@qq.com>
2022-03-27 11:55:49 +0800

    [Refactor] polish code according to pylint.

--- jmm/utilities/functions/is_url.py
+++ jmm/utilities/functions/is_url.py
@@ -4,5 +4,5 @@ def is_url(url: str) -> bool:
     try:
         result = urlparse(url)
         return all([result.scheme, result.netloc])
-    except Exception:  # pylint disable=broad-except
+    except Exception:  # pylint: disable=broad-except
         return False
